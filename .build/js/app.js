6d742c8b42efc822b0238a6b047433f7bcd54012
Quhan Arunasalam <quhan.arunasalam@gmail.com>
2015-08-24 13:44:50 +0800

    Add mock functionality to remove buttons

--- .build/js/app.js
+++ .build/js/app.js
@@ -5,71 +5,88 @@ var formData = new FormData();
 
 $(document).ready(function () {
 
-	var $pageContainer = $(':mobile-pagecontainer');
+    var $pageContainer = $(':mobile-pagecontainer');
 
-	var $uploadForm = $('#uploadform'),
-		$applicationType = $uploadForm.find('#applicationtype'),
-		$fileInput = $uploadForm.find('#file');
+    var $uploadForm = $('#uploadform'),
+        $applicationType = $uploadForm.find('#applicationtype'),
+        $fileInput = $uploadForm.find('#file');
 
-	var $uploadControls = $('#uploadcontrols'),
-		$cameraHeroBtn = $('#camerahero'),
-		$thumbnails = $uploadControls.find('#thumbnails'),
-		$cameraBtn = $uploadControls.find('#camera'),
-		$submitBtn = $uploadControls.find('#submit');
+    var $uploadControls = $('#uploadcontrols'),
+        $cameraHeroBtn = $('#camerahero'),
+        $thumbnails = $uploadControls.find('#thumbnails'),
+        $cameraBtn = $uploadControls.find('#camera'),
+        $submitBtn = $uploadControls.find('#submit');
 
-	// Attach listeners to Application Type buttons
-	$('.js-application-type').click(function () {
-		var applicationType = $(this).data('applicationtype');
+    // Attach listeners to Application Type buttons
+    $('.js-application-type').click(function () {
+        var applicationType = $(this).data('applicationtype');
 
-		if (applicationType) {
-			goToUploadPage(applicationType);
-		}
-	});
+        if (applicationType) {
+            goToUploadPage(applicationType);
+        }
+    });
 
-	$cameraHeroBtn.click(function () {
-		$fileInput.click();
-	});
+    $cameraHeroBtn.click(function () {
+        $fileInput.click();
+    });
 
-	$cameraBtn.click(function () {
-		$fileInput.click();
-	});
+    $cameraBtn.click(function () {
+        $fileInput.click();
+    });
 
-	$fileInput.change(function (event) {
-		var file = $(this).get(0).files[0];
+    $fileInput.change(function (event) {
+        var file = $(this).get(0).files[0];
 
-		if (imageType.test(file.type)) {
-			// Generate thumbnail
-			var thumbnailURL = window.URL.createObjectURL(file);
-			addThumbnail(thumbnailURL);
-		} else {
-			// TODO: Show generic file
-			addThumbnail('http://41.media.tumblr.com/16827d6a4b41ebb817c5a4a5d46baffc/tumblr_inline_nr4g6f4glB1rhj1f1_1280.png');
-		}
+        if (imageType.test(file.type)) {
+            // Generate thumbnail
+            var thumbnailURL = window.URL.createObjectURL(file);
+            addThumbnail(thumbnailURL);
+        } else {
+            // TODO: Show generic file
+            addThumbnail('http://41.media.tumblr.com/16827d6a4b41ebb817c5a4a5d46baffc/tumblr_inline_nr4g6f4glB1rhj1f1_1280.png');
+        }
 
-		formData.append('files', file);
-		showUploadControls();
-	});
+        formData.append('files', file);
+        showUploadControls();
+    });
 
-	$submitBtn.click(function () {
-		goToSummaryPage();
-	});
+    $submitBtn.click(function () {
+        goToSummaryPage();
+    });
 
-	function addThumbnail(url) {
-		var thumbnail = '<div class="thumbnail"><a href="#" class="ui-btn ui-corner-all ui-shadow ui-btn-a ui-icon-delete ui-btn-icon-notext remove">Remove</a><img src="' + url + '" /></div>';
-		$(thumbnail).appendTo($thumbnails).hide().fadeIn(250);
-	}
+    function addThumbnail(url) {
+        var uuid = guid();
+        var thumbnail = '<div class="thumbnail" id="' + uuid + '"><a href="#" class="ui-btn ui-corner-all ui-shadow ui-btn-a ui-icon-delete ui-btn-icon-notext remove-file">Remove</a><img src="' + url + '" /></div>';
+        var $thumbnail = $(thumbnail);
+        $thumbnail.appendTo($thumbnails).hide().fadeIn(250);
 
-	function showUploadControls() {
-		$cameraHeroBtn.addClass('hide');
-		$uploadControls.removeClass('hide');
-	}
+        $thumbnail.find('.remove-file').click(function () {
+            $thumbnail.fadeOut(250, function() {
+                $thumbnail.remove();
+            });
+        });
+    }
 
-	function goToUploadPage(applicationType) {
-		$applicationType.val(applicationType);
-		$pageContainer.pagecontainer('change', '#upload', {changeHash: false, transition: 'slide'});
-	}
+    function showUploadControls() {
+        $cameraHeroBtn.addClass('hide');
+        $uploadControls.removeClass('hide');
+    }
 
-	function goToSummaryPage() {
-		$pageContainer.pagecontainer('change', '#summary', {changeHash: false, transition: 'slide'});
-	}
+    function goToUploadPage(applicationType) {
+        $applicationType.val(applicationType);
+        $pageContainer.pagecontainer('change', '#upload', {changeHash: false, transition: 'slide'});
+    }
+
+    function goToSummaryPage() {
+        $pageContainer.pagecontainer('change', '#summary', {changeHash: false, transition: 'slide'});
+    }
+
+    function guid() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+    }
 });
