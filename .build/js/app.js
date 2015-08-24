e5bfc5381d90df4f7d3608dee23ce8313874a738
Quhan Arunasalam <quhan.arunasalam@gmail.com>
2015-08-24 14:50:05 +0800

    Fix upload controls on thumbnail removal & add local file array

--- .build/js/app.js
+++ .build/js/app.js
@@ -1,7 +1,9 @@
 'use strict';
 /* global $:false, document:false, window:false, FormData:false */
+
 var imageType = /^image\//;
-var formData = new FormData();
+// var formData = new FormData();
+var files = [];
 
 $(document).ready(function () {
 
@@ -37,16 +39,19 @@ $(document).ready(function () {
     $fileInput.change(function (event) {
         var file = $(this).get(0).files[0];
 
+        var uuid = guid();
+        files.push({id: uuid, file: file});
+        // formData.append('files', file);
+
         if (imageType.test(file.type)) {
             // Generate thumbnail
             var thumbnailURL = window.URL.createObjectURL(file);
-            addThumbnail(thumbnailURL);
+            addThumbnail(uuid, thumbnailURL);
         } else {
             // TODO: Show generic file
-            addThumbnail('http://41.media.tumblr.com/16827d6a4b41ebb817c5a4a5d46baffc/tumblr_inline_nr4g6f4glB1rhj1f1_1280.png');
+            addThumbnail(uuid, 'http://41.media.tumblr.com/16827d6a4b41ebb817c5a4a5d46baffc/tumblr_inline_nr4g6f4glB1rhj1f1_1280.png');
         }
 
-        formData.append('files', file);
         showUploadControls();
     });
 
@@ -54,15 +59,28 @@ $(document).ready(function () {
         goToSummaryPage();
     });
 
-    function addThumbnail(url) {
-        var uuid = guid();
+    function addThumbnail(uuid, url) {
         var thumbnail = '<div class="thumbnail" id="' + uuid + '"><a href="#" class="ui-btn ui-corner-all ui-shadow ui-btn-a ui-icon-delete ui-btn-icon-notext remove-file">Remove</a><img src="' + url + '" /></div>';
         var $thumbnail = $(thumbnail);
         $thumbnail.appendTo($thumbnails).hide().fadeIn(250);
 
         $thumbnail.find('.remove-file').click(function () {
-            $thumbnail.fadeOut(250, function() {
+            // Remove thumbnail
+            $thumbnail.fadeOut(300, function() {
                 $thumbnail.remove();
+
+                // Clean up list of files
+                for (var i = 0; i < files.length; i++) {
+                    if(files[i].id === uuid) {
+                        files.splice(i, 1);
+                        break;
+                    }
+                }
+
+                // Show the Camera Hero button if everything is reset
+                if (files.length === 0) {
+                    hideUploadControls();
+                }
             });
         });
     }
@@ -70,6 +88,11 @@ $(document).ready(function () {
     function showUploadControls() {
         $cameraHeroBtn.addClass('hide');
         $uploadControls.removeClass('hide');
+    }
+
+    function hideUploadControls() {
+        $cameraHeroBtn.removeClass('hide');
+        $uploadControls.addClass('hide');
     }
 
     function goToUploadPage(applicationType) {
