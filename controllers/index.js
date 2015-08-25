43cace18cc8f568ba91b27509f177c3497bb0c28
Quhan Arunasalam <quhan.arunasalam@gmail.com>
2015-08-25 19:53:22 +0800

    Add file renaming functionality

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,21 @@
 'use strict';
 
+var shortid = require('shortid');
+var moment = require('moment');
+
 var IndexModel = require('../models/index');
 
 var MAX_FILE_SIZE = 10 * 1024 * 1024; // 10 MB
 var SUPPORTED_IMG_MIME_TYPES = new RegExp('image\/(?=jpeg|pjpeg|gif|png)'); // JPG, GIF, PNG
 var SUPPORTED_FILE_MIME_TYPES = new RegExp(SUPPORTED_IMG_MIME_TYPES.source + '|application\/pdf|pplication\/msword|application\/vnd.openxmlformats-officedocument.wordprocessingml.document'); // ... + PDF, DOC, DOCX
 
+function getFileExtension(fileName) {
+    return fileName.substr(fileName.lastIndexOf('.')+1);
+}
+
+function generateFileName(prefix, extension) {
+    return prefix + '_' + moment().format('YYYYMMDD') + '_' + shortid.generate() + '.' + extension;
+}
 
 module.exports = function (router) {
 
@@ -24,6 +34,10 @@ module.exports = function (router) {
         var totalFileSize = 0;
 
         var images = [];
+        var nonImages = [];
+
+        // Set the file prefix based on Application Type
+        var filePrefix = (req.body && req.body.type === 'intern') ? 'INT' : 'DEV';
 
         for (var obj in files) {
             if (files.hasOwnProperty(obj)) {
@@ -41,7 +55,11 @@ module.exports = function (router) {
 
                 if (SUPPORTED_IMG_MIME_TYPES.test(file.type)) {
                     images.push(file);
+                } else {
+                    nonImages.push(file);
                 }
+
+                // console.log(generateFileName(filePrefix, getFileExtension(file.name)));
             }
         }
 
