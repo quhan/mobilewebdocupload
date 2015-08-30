3bf1a3539b4215b7f674bbb7da8af9d5d4fa871b
Quhan Arunasalam <quhan.arunasalam@gmail.com>
2015-08-30 19:03:30 +0800

    Fix typo and set HTML title

--- .build/templates/layouts/header.js
+++ .build/templates/layouts/header.js
@@ -1,1 +1,1 @@
-(function(dust){dust.register("layouts\/header",body_0);function body_0(chk,ctx){return chk.w("<header><h1>Singapore Development Center</h1></header>");}body_0.__dustBody=!0;return body_0;})(dust);
+(function(dust){dust.register("layouts\/header",body_0);function body_0(chk,ctx){return chk.w("<header><h1>Singapore Development Centre</h1></header>");}body_0.__dustBody=!0;return body_0;})(dust);
