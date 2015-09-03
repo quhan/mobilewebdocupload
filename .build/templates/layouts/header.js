f41e59f72f4aed24c9e259a44182efd4877c64a5
Quhan Arunasalam <quhan.arunasalam@gmail.com>
2015-09-04 05:49:25 +0800

    Unhinge header from content

--- .build/templates/layouts/header.js
+++
@@ -1,1 +0,0 @@
-(function(dust){dust.register("layouts\/header",body_0);function body_0(chk,ctx){return chk.w("<header><h1>Singapore Development Centre</h1></header>");}body_0.__dustBody=!0;return body_0;})(dust);
