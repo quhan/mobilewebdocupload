3825b79a196687558a43745a168c4edce2d63104
Quhan Arunasalam <quhan.arunasalam@gmail.com>
2015-09-05 07:28:30 +0800

    Peform another minor code clean up

--- .build/js/app.js
+++ .build/js/app.js
@@ -7,6 +7,15 @@ var SUPPORTED_IMG_MIME_TYPES = new RegExp('image\/(?=jpeg|pjpeg|png)'); // JPG, 
 var SUPPORTED_FILE_MIME_TYPES = new RegExp(SUPPORTED_IMG_MIME_TYPES.source + '|application\/pdf|pplication\/msword|application\/vnd.openxmlformats-officedocument.wordprocessingml.document'); // ... + PDF, DOC, DOCX
 var UPLOAD_URL = '/upload';
 
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
+
 var files = [];
 var totalFileSize = 0;
 
@@ -219,14 +228,5 @@ $(document).ready(function () {
         $introPage.addClass('hide');
         $uploadPage.addClass('hide');
         $summaryPage.removeClass('hide').addClass('slideLeft');
-    }
-
-    function guid() {
-        function s4() {
-            return Math.floor((1 + Math.random()) * 0x10000)
-                .toString(16)
-                .substring(1);
-        }
-        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
     }
 });
